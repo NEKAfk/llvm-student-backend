36d747edd0336a2721c92e98e4aecd1251c8de92
nikita <nik.zav.2005@gmail.com>
2026-03-22 11:40:50 +0300

    Add empty AsmPrinter

---
+++ llvm/lib/Target/ICD10_B69/ICD10_B69AsmPrinter.cpp
@@ -0,0 +1,61 @@
+#include "MCTargetDesc/ICD10_B69InstPrinter.h"
+#include "ICD10_B69.h"
+#include "ICD10_B69Subtarget.h"
+#include "ICD10_B69TargetMachine.h"
+#include "TargetInfo/ICD10_B69TargetInfo.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/CodeGen/AsmPrinter.h"
+#include "llvm/CodeGen/MachineConstantPool.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineInstr.h"
+#include "llvm/CodeGen/MachineModuleInfo.h"
+#include "llvm/MC/MCAsmInfo.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCStreamer.h"
+#include "llvm/MC/MCSymbol.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "asm-printer"
+
+namespace {
+
+class ICD10_B69AsmPrinter : public AsmPrinter {
+  const MCSubtargetInfo *STI;
+
+public:
+  explicit ICD10_B69AsmPrinter(TargetMachine &TM,
+                         std::unique_ptr<MCStreamer> Streamer)
+      : AsmPrinter(TM, std::move(Streamer)), STI(TM.getMCSubtargetInfo()) {
+    ICD10_B69_DUMP_GREEN
+  }
+
+  void emitInstruction(const MachineInstr *MI) override;
+
+  StringRef getPassName() const override { return "ICD10_B69 Assembly Printer"; }
+
+  bool lowerPseudoInstExpansion(const MachineInstr *MI, MCInst &Inst);
+};
+
+} // end anonymous namespace
+
+// Simple pseudo-instructions have their lowering (with expansion to real
+// instructions) auto-generated.
+#include "ICD10_B69GenMCPseudoLowering.inc"
+
+void ICD10_B69AsmPrinter::emitInstruction(const MachineInstr *MI) {
+  ICD10_B69_DUMP_GREEN
+  // Do any auto-generated pseudo lowerings.
+  if (MCInst OutInst; lowerPseudoInstExpansion(MI, OutInst)) {
+    EmitToStreamer(*OutStreamer, OutInst);
+    return;
+  }
+}
+
+// Force static initialization.
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeICD10_B69AsmPrinter() {
+  RegisterAsmPrinter<ICD10_B69AsmPrinter> X(getTheICD10_B69Target());
+}
