5881812c18797e1b1d8ba5bddfcd70a6b962d8ad
nikita <nik.zav.2005@gmail.com>
2026-04-12 11:23:30 +0300

    Add ICD10_B69 ClangBuiltin support (flush and putpixel and rndmod)

--- clang/include/clang/Basic/TargetBuiltins.h
+++ clang/include/clang/Basic/TargetBuiltins.h
@@ -402,12 +402,23 @@ namespace clang {
     };
   }
 
+
+  /// ICD10_B69 builtins
+  namespace ICD10_B69 {
+    enum {
+        LastTIBuiltin = clang::Builtin::FirstTSBuiltin-1,
+#define BUILTIN(ID, TYPE, ATTRS) BI##ID,
+#include "clang/Basic/BuiltinsICD10_B69.def"
+        LastTSBuiltin
+    };
+  }
+  
   static constexpr uint64_t LargestBuiltinID = std::max<uint64_t>(
       {ARM::LastTSBuiltin, AArch64::LastTSBuiltin, BPF::LastTSBuiltin,
        PPC::LastTSBuiltin, NVPTX::LastTSBuiltin, AMDGPU::LastTSBuiltin,
        X86::LastTSBuiltin, VE::LastTSBuiltin, RISCV::LastTSBuiltin,
        Hexagon::LastTSBuiltin, Mips::LastTSBuiltin, XCore::LastTSBuiltin,
-       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin});
+       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin, ICD10_B69::LastTSBuiltin});
 
 } // end namespace clang.
 
--- clang/lib/Basic/Targets/ICD10_B69.cpp
+++ clang/lib/Basic/Targets/ICD10_B69.cpp
@@ -1,9 +1,16 @@
 #include "ICD10_B69.h"
 #include "clang/Basic/Builtins.h"
 #include "clang/Basic/MacroBuilder.h"
+#include "clang/Basic/TargetBuiltins.h"
 
 using namespace clang;
 using namespace clang::targets;
+
+static constexpr Builtin::Info BuiltinInfo[] = {
+#define BUILTIN(ID, TYPE, ATTRS)                                               \
+  {#ID, TYPE, ATTRS, nullptr, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
+#include "clang/Basic/BuiltinsICD10_B69.def"
+};
 
 void ICD10_B69TargetInfo::getTargetDefines(const LangOptions &Opts,
                                      MacroBuilder &Builder) const {
@@ -11,5 +18,6 @@ void ICD10_B69TargetInfo::getTargetDefines(const LangOptions &Opts,
 }
 
 ArrayRef<Builtin::Info> ICD10_B69TargetInfo::getTargetBuiltins() const {
-  return std::nullopt;
+  return llvm::ArrayRef(BuiltinInfo,
+                        clang::ICD10_B69::LastTSBuiltin - Builtin::FirstTSBuiltin);
 }
